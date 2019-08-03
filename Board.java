7a118bd473743c63e66033365c0e29f80c1a7e6d
Michael Antonacci <53633068+Pandamike118@users.noreply.github.com>
2019-08-03 10:21:48 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -9,8 +9,6 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-import gamecore.Emenies.Skeleton;
-import gamecore.Towers.Archer.Archer;
 
 public class Board extends Canvas implements Runnable{
 	private static final long serialVersionUID = 3485185195587775746L;
@@ -19,33 +17,10 @@ public class Board extends Canvas implements Runnable{
 	public static final int HEIGHT = 832; // Setting the size of the window
 	private boolean running = false;// stating that running is equal to false
 	private Handler handler; // stating a handler class that will take care of all the objects in the game
-	private HUD hud; // stating the Health Up Display
-	private Generate generate; // stating the wave generator
-	private Menu menu; // stating the menu screen
-	private Shop shop;
+	
 	public static BufferedImage spread_Sheet; 
 	protected BufferedImage Image;
 	
-	
-
-	public static int[][] newMap = { 
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-			{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-			{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-			{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-			{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-			{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 } };
-	// this is the game map path
-
-	public static PathGrid Path = new PathGrid(newMap);// we are creating the game map and converting the 1 and 0's to
-														// actually 64 x 64 tile with ID's
 
 	// the main method which will call upon our board method which has everything
 	// inside
@@ -54,15 +29,6 @@ public class Board extends Canvas implements Runnable{
 
 	}
 
-	// this enum tell us the state if the game if it is either on the Menu "page" or
-	// the Game "page" etc.
-	public enum STATE {
-		Menu, Game;
-	}
-
-	public STATE gameState = STATE.Menu;// stating the game state as Menu (the start of the game)
-	public static Tile startTile = gamecore.PathGrid.Map[0][3]; // stating the start tile for all enemies
-	
 	/**
 	 * Our Most important method this will load everything that the game will need
 	 * to run and create all the enemies and other thing in side the game
@@ -70,21 +36,14 @@ public class Board extends Canvas implements Runnable{
 	public Board() {
 		handler = new Handler(); // init. a new handler
 
-		menu = new Menu(this, handler); // init. the menu
 		
-		this.addMouseListener(menu);
 
-		new Window(WIDITH, HEIGHT, "Tower Defense Game", this); // initizies the window
+		new Window(WIDITH, HEIGHT, "plateformer", this); // initizies the window
 		
 		BufferedImageLoader loader = new BufferedImageLoader(); 
 		
-		spread_Sheet = loader.getImage("/64SpreadSheet.png"); // loads the the spread sheet for the graphics
+		spread_Sheet = loader.getImage("Enter spreadsheet"); // loads the the spread sheet for the graphics
 		Image = spread_Sheet.getSubimage(1, 1, 64, 64); // getting specific single 64x64 frame 
-		hud = new HUD(); // init. the Health Up Display
-
-		shop = new Shop(this, handler);
-		this.addMouseListener(shop);
-		generate = new Generate(handler, hud);
 		
 	}
 
@@ -159,17 +118,8 @@ public class Board extends Canvas implements Runnable{
 	 * the tick methods in the objects that in the game
 	 */
 	private void tick() {
-		// if we change the game state to game then it will run the tick method that
-		// hold all thing the "game" needs
-		if (gameState == STATE.Game) {
 			handler.tick(); // run the tick method in the handler class
-			hud.tick();// run the tick method in the Health Up Display class
-			shop.tick();
-			generate.tick(); //generate.tick(); // run the tick method in the Wave Generate
-
-		} else {
-			menu.tick(); // run the tick method in the menu class
-		}
+			
 	}
 
 	/*
@@ -189,33 +139,6 @@ public class Board extends Canvas implements Runnable{
 
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDITH, HEIGHT);// this will create the background to black
-
-		if (gameState == STATE.Game) {
-			for (int i = 0; i < gamecore.PathGrid.Map.length; i++) {
-				for (int j = 0; j < gamecore.PathGrid.Map[i].length; j++) {
-					Tile temp = gamecore.PathGrid.Map[i][j];
-					// this will check if the the map grid is either 1 2 or 0 and will change the
-					// color which then
-					// will create the map
-					if (temp.getID() == tileID.Grass) {
-						g.drawImage(Image, (int)temp.getX(),  (int)temp.getY(), null);
-					} else if (temp.getID() == tileID.Path) {
-						
-					} else if (temp.getID() == tileID.Water) {
-						//Image = spread_Sheet.getSubimage(3,1, 64, 64);
-					}
-					
-				}
-
-				handler.render(g);// this will render all the objects in the game (if they are Game objects)
-				shop.render(g);
-				hud.render(g);// this will render the Health Up Display
-
-			}
-		} else {
-			menu.render(g); // this will render the menu display
-		}
-
 		g.dispose(); // we will dispose of the old graphics from previous frame
 		Buffer.show(); // will show the new updated graphics
 	}
