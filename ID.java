c74c3fa82c5922c46b61d7ee6a0d0044cd53a465
Michael Antonacci <53633068+Pandamike118@users.noreply.github.com>
2019-08-04 21:03:56 -0400

    Enum for object ID
    this document is where all object ID is declared so when doing for loop and trying to affect specific object we can call upon the ID of the object.

---
+++ ID.java
@@ -0,0 +1,22 @@
+package gamecore;
+/**
+ * this ENUM create the ID for all objects in the game 
+ * @author michaelantonacci
+ *
+ */
+public enum ID {
+	Warrior(),
+	Skeleton(), 
+	Archer(),
+	Catapult(), 
+	Mage(), 
+	Hercule(), 
+	Path(),
+	Arrow(), 
+	Sword(),
+	Rock(),
+	BigSkeleton(),
+	ArmorSkeleton(); 
+	
+	
+}
